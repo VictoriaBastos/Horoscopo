8745f81bfd08947cd51ba9cf2aa9705eac05a10a
VictoriaBastos <victoria.rezbastos@gmail.com>
2022-09-29 10:51:02 -0700

    chore: adiciona main class aplication

---
+++ src/main/java/br/com/rezebas/signos/SignosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rezebas.signos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignosApplication.class, args);
+	}
+
+}
