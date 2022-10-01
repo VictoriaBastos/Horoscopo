a83d9b0ce810ec807c5da45d4e821b62cc6656ae
VictoriaBastos <victoria.rezbastos@gmail.com>
2022-09-30 18:18:46 -0700

    chore: altera nome request requisicao

--- src/main/java/br/com/rezebas/signos/dto/MapaAstralRequest.java
+++ src/main/java/br/com/rezebas/signos/dto/MapaAstralRequest.java
@@ -9,31 +9,31 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class HoroscopoRequest {
+public class MapaAstralRequest {
 
     @NotBlank(message = "Campo nome e obrigatorio.")
     private String nome;
 
-    @Max(value = 4)
+    @Max(value = 2022)
     @NotNull(message = "Campo ano de nascimento e obrigatorio.")
     @JsonProperty(value = "ano_nascimento")
     private Integer anoNascimento;
 
-    @Max(value = 2)
+    @Max(value = 12)
     @NotNull(message = "Campo mes de nascimento e obrigatorio.")
     @JsonProperty(value = "mes_nascimento")
     private Integer mesNascimento;
 
-    @Max(value = 2)
+    @Max(value = 31)
     @NotNull(message = "Campo dia de nascimento e obrigatorio.")
     @JsonProperty(value = "dia_nascimento")
     private Integer diaNascimento;
 
-    @NotBlank(message = "Campo hora de nascimento e obrigatorio.")
+    @NotNull(message = "Campo hora de nascimento e obrigatorio.")
     @JsonProperty(value = "hora_nascimento")
     private Integer horaNascimento;
 
-    @NotBlank(message = "Campo minuto de nascimento e obrigatorio.")
+    @NotNull(message = "Campo minuto de nascimento e obrigatorio.")
     @JsonProperty(value = "minutos_nascimento")
     private Integer minutosNascimento;
 
