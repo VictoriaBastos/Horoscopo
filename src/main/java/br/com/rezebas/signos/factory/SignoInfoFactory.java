695741fa5eff1da375a1210b78066f9d72b8fd8b
VictoriaBastos <victoria.rezbastos@gmail.com>
2022-09-30 18:16:20 -0700

    chore: adiciona factory para auxiliar recuperacao de signo ascendente

--- src/main/java/br/com/rezebas/signos/factory/SignoInfoFactory.java
+++ src/main/java/br/com/rezebas/signos/factory/SignoInfoFactory.java
@@ -1,21 +1,34 @@
 package br.com.rezebas.signos.factory;
 
-import br.com.rezebas.signos.model.Aquario;
-import br.com.rezebas.signos.model.Aries;
-import br.com.rezebas.signos.model.Signo;
+import br.com.rezebas.signos.model.*;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class SignoInfoFactory {
 
-    private final Map<String, Signo> signoInfo = Map.of(
-      "Aries", new Aries(),
-      "Aquario", new Aquario()
-    );
+    private Map<String, Signo> signoMap;
 
-    public Signo getSignoInfo(String signoSolar){
-        return signoInfo.get(signoSolar);
+    public SignoInfoFactory(){
+        signoMap = new HashMap<>();
+        signoMap.put("Aries", new Aries());
+        signoMap.put("Touro", new Touro());
+        signoMap.put("Gemeos", new Gemeos());
+        signoMap.put("Cancer", new Cancer());
+        signoMap.put("Leao", new Leao());
+        signoMap.put("Virgem", new Virgem());
+        signoMap.put("Libra", new Libra());
+        signoMap.put("Escorpiao", new Escorpiao());
+        signoMap.put("Sagitario", new Sagitario());
+        signoMap.put("Peixes", new Peixes());
+        signoMap.put("Capricornio",new Capricornio());
+        signoMap.put("Aquario",new Aquario());
+    }
+
+
+    public Signo getSignoInfo(String signoSolar) {
+        return signoMap.get(signoSolar);
     }
 }
